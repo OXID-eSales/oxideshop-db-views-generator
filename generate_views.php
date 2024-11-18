0567ce5406e99765613757d0021059fb60b0fa45
Vasyl Liulka <vasyl.liulka@oxid-esales.com>
2024-11-18 15:20:54 +0100

    OXDEV-7248 Fix copyright headers

--- generate_views.php
+++ generate_views.php
@@ -1,27 +1,11 @@
 <?php
+
 /**
- * This file is part of OXID eSales Views Generator.
- *
- * OXID eSales Views Generator is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * OXID eSales Views Generator is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with OXID eSales Views Generator. If not, see <http://www.gnu.org/licenses/>.
- *
- * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2017
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
  */
 
 namespace OxidEsales\DatabaseViewsGenerator;
-
-use OxidEsales\DatabaseViewsGenerator\ViewsGenerator;
 
 $bootstrapFileName = getenv('ESHOP_BOOTSTRAP_PATH');
 if (!empty($bootstrapFileName)) {
